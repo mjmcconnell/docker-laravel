f35fdc04bd1b9d679edc5a21654a88cb1bfc3d47
Mark McConnell <mjmcconnell.dev@gmail.com>
2017-05-29 19:59:34 +0100

    debug toolbar added

--- app/config/app.php
+++ app/config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
